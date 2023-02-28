541909cd30f9cb47fd82f305598e52a2ae7a8c16
Lizi-Sakhokia <lizisakhokia17>
2023-03-01 01:01:38 +0400

    Homework

---
+++ js/natura.js
@@ -0,0 +1,27 @@
+
+let anteba = () => {
+    document.getElementById("natura").src = "img/ON.png"
+}
+
+let chaqroba = () => {
+    document.getElementById("natura").src = "img/OFF.png"
+}   
+
+// let universal = () =>{
+//     let address = document.getElementById("natura").src;
+
+//     console.log(address)
+
+//     if(address ==""){
+//         document.getElementById("natura").src = "img/ON.png"
+//     }
+//     else{
+//         document.getElementById("natura").src = "img/OFF.png"
+//     }
+// }
+
+let misamarti = () =>{
+    let address = document.getElementById("button").src
+    
+    console.log(address)
+}
